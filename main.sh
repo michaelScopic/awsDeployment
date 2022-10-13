6d419d16865dc844afa507a50bdf089c1ab83ed8
Michael_Scopic <michael@sanbornhome.com>
2022-10-13 21:02:44 +0000

    added `setup.sh`

--- main.sh
+++ main.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env sh
+# !! NOTES:
+#   -- !! This script is made for Debian-based x86_64 machines. !! --
+#
+#       You will manually need to change the script to adapt it to other
+#       distros or architectures.
 
 echo "\e[31m -- Initalizing script... -- \e[0m" && sleep 0.2
 # - -gives me an excuse to get info of distro and shell idk
-
+echo ""
 # --- Print out basic info ---
+echo "\e[35m---------- Basic info ----------\e[0m"
 # Print the distro 
-echo "\e[32mDistro: \e[0m$(lsb_release -d | awk '{print $2}' )"
+echo   "\e[32mDistro: \e[0m$(lsb_release -d | cut -f 2- )"
 # Print kernel version
-echo "\e[33mKernel: \e[0m$(uname -r)" 
-# Show shell 
-echo "\e[34mShell: \e[0m$SHELL" 
+echo "\e[33mKernel: \e[0m$(uname -r) $(uname -o)" 
+# Print shell 
+echo "\e[34mShell: $SHELL \e[0m"
+# Print hostname
+echo "\e[35mHostname: \e[0m$(cat /etc/hostname)"
+# Print current user (is user root?)
+echo "\e[36mUser: \e[0m$(whoami)"
 echo "\e[35m--------------------------------\e[0m"
 sleep 1
---
+++ setup.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+# !! NOTES:
+#   -- !! This script is made for Ubuntu x86_64 machines. !! --
+#
+#       You will manually need to change the script to adapt it to other
+#       distros or architectures.
+
+echo -e "
++----------------------------------------+
+| \e[32mUsing '\e[33mapt\e[32m' to install dependancies...\e[0m |
++----------------------------------------+"
+sleep 0.5
+
+# ---- Update apt repositories and then upgrade local packages ----
+sudo apt update && \
+sudo apt upgrade -y && \
+echo -e "\e[33m --- Note: updated/upgraded packages... --- \e[0m"
+
+# ---- Install dependancies ---- 
+echo -e "
++-----------------------------+
+| \e[33mInstalling dependancies...\e[0m  |
+| \e[32m Depenancy list: \e[0m           |
+|  \e[32m docker \e[0m                   |   
+|  \e[32m cockpit \e[0m                  |
+|  \e[32m git \e[0m                      |
+|  \e[32m zsh \e[0m                      |
++-----------------------------+
+" 
+sleep 2
+# -- Docker --
+# Remove old docker versions
+echo -e "
++---------------------------------------+
+| \e[31mRemoving old Docker versions (if any)\e[0m |
++---------------------------------------+" 
+sleep 1
+#sudo apt-get remove docker docker-engine docker.io containerd runc
+# Installing Docker
+echo -e "
++----------------------+
+| \e[33mInstalling Docker... \e[0m|
++----------------------+" 
+sleep 0.5
+
+#sudo apt-get install \
+#    ca-certificates \
+#    curl \
+#    gnupg \
+#    lsb-release
+
+#sudo mkdir -p /etc/apt/keyrings
+
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+#echo \
+#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#sudo apt-get update && \
+#sudo apt-get install \
+#    docker-ce \ 
+#    docker-ce-cli \
+#    containerd.io \ 
+#    docker-compose-plugin
+
+# Add user to docker group
+#sudo groupadd docker
+#sudo usermod -aG docker $USER && \
+echo -e "
++---------------------------------------+
+| \e[32mInstalled Docker!\e[0m                     |
+| \e[32mAdded user to docker group! \e[0m          |
+|                                       |
+| \e[33mYou should exit and log back in again \e[0m|
+| \e[33mfor the group change to take effect.\e[0m  |
++---------------------------------------+"
+sleep 2
+
+# Install cockpit
+
+# Install shell tools
+
