22d2f09abc5e09dc207c023c341a039b70675875
Michael_Scopic <67214805+michaelScopic@users.noreply.github.com>
2022-10-13 18:49:11 -0700

    uncommented lines for testing

--- setup.sh
+++ setup.sh
@@ -36,7 +36,7 @@ echo -e "
 | \e[31mRemoving old Docker versions (if any)\e[0m |
 +---------------------------------------+" 
 sleep 1
-#sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get remove docker docker-engine docker.io containerd runc
 # Installing Docker
 echo -e "
 +----------------------+
@@ -44,30 +44,30 @@ echo -e "
 +----------------------+" 
 sleep 0.5
 
-#sudo apt-get install \
-#    ca-certificates \
-#    curl \
-#    gnupg \
-#    lsb-release
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
-#sudo mkdir -p /etc/apt/keyrings
+sudo mkdir -p /etc/apt/keyrings
 
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-#echo \
-#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-#sudo apt-get update && \
-#sudo apt-get install \
-#    docker-ce \ 
-#    docker-ce-cli \
-#    containerd.io \ 
-#    docker-compose-plugin
+sudo apt-get update && \
+sudo apt-get install \
+    docker-ce \ 
+    docker-ce-cli \
+    containerd.io \ 
+    docker-compose-plugin
 
 # Add user to docker group
-#sudo groupadd docker
-#sudo usermod -aG docker $USER && \
+sudo groupadd docker
+sudo usermod -aG docker $USER && \
 echo -e "
 +---------------------------------------+
 | \e[32mInstalled Docker!\e[0m                     |
