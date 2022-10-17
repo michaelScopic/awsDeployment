4292d1625817312659621a69cc0fca6b5c179a35
michaelScopic <michael@sanbornhome.com>
2022-10-17 21:12:05 +0000

    balls.

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # !! NOTES:
-#   -- !! This script is made for Debian-based x86_64 machines. !! --
+#   -- !! This script is made for Ubuntu based x86_64 machines. !! --
 #
 #       You will manually need to change the script to adapt it to other
 #       distros or architectures.
